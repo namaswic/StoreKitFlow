f024a7181e7dc53b13917cf8378767accc487e17
Namaswi Chandarana <namaswic@gmail.com>
2026-05-10 18:24:43 -0400

    Add DemoApp Xcode project scaffold

---
+++ DemoApp/StoreKitFlowDemo/StoreKitFlowDemo/StoreKitFlowDemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  StoreKitFlowDemoApp.swift
+//  StoreKitFlowDemo
+//
+//  Created by Namaswi Chandarana on 5/10/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StoreKitFlowDemoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DemoApp/StoreKitFlowDemo/StoreKitFlowDemoTests/StoreKitFlowDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKitFlowDemoTests.swift
+//  StoreKitFlowDemoTests
+//
+//  Created by Namaswi Chandarana on 5/10/26.
+//
+
+import Testing
+@testable import StoreKitFlowDemo
+
+struct StoreKitFlowDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
